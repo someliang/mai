b3c5c208b5e58eb3739177e0f9a6170aaf196434
someLiang <viso.liang@qq.com>
2012-01-12 23:48:29 +0800

    1.添加初始化配置文件,local_settings.py 2.初始化数据库环境.

---
+++ mai/local_settings.py
@@ -0,0 +1,15 @@
+'''
+Created on Jan 12, 2012
+
+@author: someLiang
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mai',                      # Or path to database file if using sqlite3.
+        'USER': 'kaka',                      # Not used with sqlite3.
+        'PASSWORD': '888888',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
